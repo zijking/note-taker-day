d07bc902049704bf051039779b449482071aecee
ZijKing <zi.12feodor@gmail.com>
2022-11-25 10:44:01 +0200

    add comit

---
+++ src/components/Heder/Header.js
@@ -0,0 +1,11 @@
+import style from './Header.module.css';
+
+function Header() {
+  return (
+    <header>
+      <h2 className={style.header}>Hello</h2>
+    </header>
+  );
+}
+
+export default Header;
